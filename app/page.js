906f6224afa59a6054043b80482cde5c62889868
Sugini Injani <forggdev@gmail.com>
2025-04-08 17:02:43 +0700

    commit project

---
+++ app/page.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col bg-[#03071E]">
+      <Navbar/>
+      <div className="container mx-auto mt-24 px-12 py-4">
+        <HeroSection/>
+      </div>
+    </main>
+  );
+}
